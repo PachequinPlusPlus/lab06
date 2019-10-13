b138f1c65b7bdefc71f4205b3b69de64873ac0a0
pachequinplusplus <samuel-1998@live.com>
2019-10-13 11:17:04 -0500

    added everything

---
+++ index.js
@@ -0,0 +1,96 @@
+var url = "https://www.googleapis.com/youtube/v3/search";
+var apiKey = "AIzaSyBxiJKCFxR-vLxoR2j_zkDBxE_QYZVshfg";
+var nxtToken, prevToken;
+function main(){
+
+    $("#go").on("click", function(e){
+        e.preventDefault();
+        var videoName = $("#search").val();
+        $("#videos").html("");
+
+        $.ajax({
+            url: url,
+            data : {
+                key : apiKey,
+                q : "cat",
+                part: "id, snippet",
+                maxResults : 10,
+                type : "video",
+            },
+            dataType : "json",
+            type : "GET",
+            success : function(response){
+                    loadData(response);
+                    fixButton(response); 
+            },
+            error : function(err){
+                console.log(err);
+            }
+        });
+    });
+
+
+    $("button").on("click", function(e){
+        e.preventDefault();
+        $("#videos").html("");
+        var tk;
+        if(e.target.id === "n"){
+            tk = nxtToken;
+        }else{
+            tk = prevToken;
+        }
+        $.ajax({
+            url : url,
+            data : {
+                key : apiKey,
+                q : "cat",
+                part: "id, snippet",
+                maxResults : 10,
+                type : "video",
+                pageToken : tk,
+            },
+            dataType : "json",
+            type : "GET",
+            success : function(response){
+                loadData(response);
+                fixButton(response);
+            },
+            error : function(err){
+                console.log(err);
+            }
+        });
+    });
+}
+
+function loadData(response){
+    console.log(response);
+                    response.items.forEach((item)=>{
+                    var element = $(`<li>
+                                       <p>${item.snippet.title}</p>  
+                                        <a target="_blank" href="https://www.youtube.com/watch?v=${item.id.videoId}">
+                                         <img src="${item.snippet.thumbnails.default.url}">
+                                       </a> 
+                                    </li>`);
+                    $("#videos").append(element);
+                });
+}
+
+function fixButton(response){
+            if(response.nextPageToken){
+                    $("#n").attr("class", "");
+                    nxtToken = response.nextPageToken;
+                }else{
+                    $("#n").attr("class", "hide");
+                    nxtToken = "";
+                }
+
+                if(response.prevPageToken){
+                    $("#b").attr("class", "");
+                    prevToken = response.prevPageToken;
+                }else{
+                    $("#b").attr("class", "hide");
+                    prevToken = "";
+                }
+}
+
+main();
